cae9761b333d206fdfcd10c36d68aabfb4d947d8
BritnessDev <britness.gmd@gmail.com>
2022-10-15 09:05:50 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center">
+      <div className="app-body">
+         <div>Header</div>
+         {/* content */}
+         <div className='flex flex-row justify-center items-start'> 
+              <div>
+                search List boxes
+              </div>
+              <div>
+                <div>
+                  filter
+                </div>
+                <div>
+                  status
+                </div>
+                <div>
+                  main car list
+                </div>
+              </div>
+         </div>
+      </div>
+         
     </div>
   );
 }
